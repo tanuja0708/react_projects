5d602a61fbad36db980534e0624b619e6bb4898f
tanuja0708 <tanujapandey398@gmail.com>
2026-06-19 23:59:32 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-// import Evpage from './components/ev-website/evpage';
-import Landingpage from './components/amazonlandingpage/landingpage';
+import {useState} from 'react';
+import Background from './components/Background/Background';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
-function App() {
-  return (
-    <>
-      <h1>Main App</h1>
-      <Evpage />
-      {/* <Landingpage /> */}
-    </>
-  )
+
+const App = () => {
+    let heroData = [
+        {text1: "Dive into", text2: "what you love"},
+        {tesx1: "Indulge", text2: "your passions"},
+        {tesx1: "Give in to", text2: "your passions"},
+    ]
+    const [heroCount,setHeroCount] = useState(0);
+    const [playStatus, setPlayStatus] = useState(false);
+    return (
+        <div>
+            <Background playStatus = {playStatus} heroCount = {heroCount} />
+            <Navbar/>
+            <Hero 
+            setPlayStatus={setPlayStatus}
+            heroData={heroData}
+            heroCount={herCount}
+            setHeroCount={setHeroCount}
+            playStatus={playStatus}
+            />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css';
+import arrow_btn from '../../assets/arrow_btn.png';
+import play_icon from '../../assets/play_icon.png';
+import pause_icon from '../../assets/pause_icon.png';
+
+const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+    return (
+        <div className='hero'>
+            <div className='hero-text'>
+                
+            </div>
+        </div>
+    )
+}
+export default Hero;
