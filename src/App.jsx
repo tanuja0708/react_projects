5baa27aebdf7913630111d4202f19a7b36e47749
tanuja0708 <tanujapandey398@gmail.com>
2026-06-20 21:49:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Background from './components/Background/Background';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
@@ -12,14 +12,20 @@ const App = () => {
     ]
     const [heroCount,setHeroCount] = useState(0);
     const [playStatus, setPlayStatus] = useState(false);
+
+    useEffect(()=>{
+        setInterval(() => {
+            setHeroCount((count)=>{return count===2?0:count+1})
+        },3000);
+    },[])
     return (
         <div>
             <Background playStatus = {playStatus} heroCount = {heroCount} />
             <Navbar/>
             <Hero 
             setPlayStatus={setPlayStatus}
-            heroData={heroData}
-            heroCount={herCount}
+            heroData={heroData[heroCount]}
+            heroCount={heroCount}
             setHeroCount={setHeroCount}
             playStatus={playStatus}
             />
